11602bca5abde4ac56a029fa2fcc39a72422f30f
Jesus Mezquiti <jmgcode@gmail.com>
2023-03-03 10:28:09 -0600

    feat: remove files from the form feat: add styles for mobile feat: send email with sendContactForm api

--- app/home/info/index.tsx
+++ app/home/info/index.tsx
@@ -1,220 +1,113 @@
-import { ChangeEvent, forwardRef, useState } from "react";
+import { useRef, useState } from "react";
 
 import Button from "../../../components/Button/Button";
-import Document from "../../../components/Icons/Document/Document";
 import Input from "../../../components/Input/Input";
-import Upload from "../../../components/Icons/upload";
-import { primaryColor } from "../../../abstracts/colors";
+import Modal from "../../../components/Modal/Modal";
+import Portal from "../../../HOC/Portal";
+import { sendContactForm } from "../../../lib/api";
 import styles from "./info.module.scss";
 
-//TODO: Fix ref typescript type
-// eslint-disable-next-line react/display-name
-const Info = forwardRef((props, ref) => {
-  const [formState, setFormState] = useState<{
-    name: string;
-    email: string;
-    message: string;
-    files: any[];
-  }>({
+const Info = () => {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const detailsRef = useRef(null);
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
-    message: "",
-    files: [],
+    details: "",
   });
 
-  const handleSubmit = (e: any) => {
+  const handleSetData = (e: any) => {
+    const { id, value } = e.currentTarget;
+    setFormData((prev) => ({ ...prev, [id]: value }));
+  };
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
+    let a = true,
+      b = true,
+      c = true;
+    //@ts-ignore
+    if (nameRef.current && !nameRef.current.validate()) a = false;
+    //@ts-ignore
+    if (emailRef.current && !emailRef.current.validate()) b = false;
+    //@ts-ignore
+    if (detailsRef.current && !detailsRef.current.validate()) c = false;
 
-    const formData = new FormData();
-    Object.keys(formState).map((objKey) => {
-      if (objKey === "files") {
-        formState[objKey].forEach((file) => {
-          formData.append("files", file);
-        });
-      } else {
-        //@ts-ignore
-        formData.append(e, formState[objKey]);
+    if (a && b && c) {
+      try {
+        await sendContactForm(formData);
+        console.log("sended successfully");
+      } catch (error: any) {
+        console.log("Submit error:", error.message);
       }
-    });
-
-    //@ts-ignore
-    for (const value of formData.values()) {
-      console.log(value);
     }
-
-    console.log("typeof :", typeof formData);
-    fetch("http://localhost:3000/api/contact", {
-      method: "POST",
-      headers: { "Content-Type": "multipart/form-data" },
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-
-    // fetch("https://formsubmit.co/ajax/9c8a2031859cb956dd7fcf6c5ba4785d", {
-    //   method: "POST",
-    //   headers: {
-    //     // "Content-Type": "application/json",
-    //     "Content-Type": "multipart/form-data",
-    //     Accept: "*/*",
-    //     // Accept: "application/json",
-    //   },
-    //   // body: JSON.stringify(formState),
-    //   body: formData,
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .catch((error) => console.log(error));
   };
 
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setFormState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
   return (
-    //@ts-ignore
-    <div ref={ref} className={styles.container} id="info-section">
+    <div
+      className={styles.container}
+      id="contact-section"
+      style={{ position: "relative" }}
+    >
       <div className={styles.text}>
-        <h2 className="font-medium-bold">{`I'm currently available for freelance work`}</h2>
+        <h2 className="font-medium-bold">{`Go a project in mind?`}</h2>
         <p>
-          {`If you are looking for a develop that like to get stuff done, let's
-          talk.`}
+          {`Use the form to give me as much detail as possible and I'll get back to you as soon as I can.`}
         </p>
-
-        <form
-          encType="multipart/form-data"
-          onSubmit={handleSubmit}
-          style={{ display: "flex", flexDirection: "column", rowGap: "20px" }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "42px",
-              padding: "20px 0",
-            }}
-          >
-            <Input
-              label="Name"
-              id="name"
-              placeholder="Enter your name"
-              required
-              onChange={handleChange}
-              value={formState.name}
-            />
-            <Input
-              label="Email"
-              id="email"
-              placeholder="Enter your email"
-              required
-              onChange={handleChange}
-              value={formState.email}
-            />
-            <Input
-              label="Message"
-              id="message"
-              placeholder="Enter your message"
-              required
-              rows={3}
-              onChange={handleChange}
-              value={formState.message}
-            />
-            <input
-              type="file"
-              name="attachment"
-              id="attachment"
-              multiple
-              accept="image/*,application/pdf,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-              className={styles.inputfile}
-              onChange={(e) => {
-                console.log("coca cola light", e.target.files);
-                //@ts-ignore
-                const fileArr = Object.keys(e.target.files).map((file) => {
-                  //@ts-ignore
-                  return e.target.files[file];
-                });
-                setFormState((prev) => ({
-                  ...prev,
-                  files: [...fileArr, ...prev.files],
-                }));
-              }}
-            />
-
-            <label className={styles.inputlabel} htmlFor="attachment">
-              <Upload color={primaryColor} size="100" />
-              Browse file to upload
-            </label>
-
-            {formState.files.length > 0 && (
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "8px",
-                }}
-              >
-                {formState.files.map((file, index) => {
-                  const docType = file.type.includes("image")
-                    ? "img"
-                    : file.type.includes("pdf")
-                    ? "pdf"
-                    : "doc";
-
-                  return (
-                    <div
-                      key={file.name + "-" + index}
-                      style={{
-                        display: "flex",
-                        gap: "15px",
-                        // backgroundColor: "rgba(252, 58, 121, 1)",
-                        background:
-                          "linear-gradient(90deg,rgba(254, 87, 46, 1) 0%,rgba(252, 58, 121, 1) 100%)",
-                        padding: "15px 20px",
-                        borderRadius: "8px",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                      }}
-                    >
-                      <div
-                        style={{
-                          display: "flex",
-                          gap: "10px",
-                          alignItems: "center",
-                        }}
-                      >
-                        <Document icon={docType} />
-                        <p style={{ color: "white", fontWeight: "bold" }}>
-                          {file.name}
-                        </p>
-                      </div>
-                      <button
-                        style={{
-                          backgroundColor: "transparent",
-                          border: "none",
-                        }}
-                        onClick={() => {
-                          const files = formState.files;
-                          files.splice(index, 1);
-
-                          setFormState((prev) => ({
-                            ...prev,
-                            files,
-                          }));
-                        }}
-                      >
-                        <p style={{ color: "white", cursor: "pointer" }}>X</p>
-                      </button>
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-          <Button isSubmit text="Let's talk!" onClick={() => {}} />
-        </form>
       </div>
+      <form className={styles["form-container"]} onSubmit={handleSubmit}>
+        <Input
+          id="name"
+          label="Your name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleSetData}
+          validation={{
+            rules: [/^[a-zA-ZÀ-ÖØ-öø-ÿ '-]+$/],
+            msg: "Please enter your name",
+          }}
+          ref={nameRef}
+          required
+        />
+        <Input
+          id="email"
+          label="Your email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleSetData}
+          validation={{
+            rules: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/],
+            msg: "Please enter a valid email",
+          }}
+          ref={emailRef}
+          required
+        />
+        <div className={styles["col-2"]}>
+          <Input
+            id="details"
+            label="Your Project Details"
+            placeholder="Project Details"
+            rows={10}
+            value={formData.details}
+            onChange={handleSetData}
+            ref={detailsRef}
+            required
+          />
+          <Button
+            isSubmit
+            className={`  ${styles["form-button"]}`}
+            text="Send message"
+            onClick={() => {}}
+          />
+        </div>
+      </form>
+
+      {/* <Portal>
+        <Modal />
+      </Portal> */}
     </div>
   );
-});
+};
 
 export default Info;
