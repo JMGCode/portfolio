62bdbab62847ee96f2b2aa465ce15186f145af36
Jesus Mezquiti <jmgcode@gmail.com>
2023-03-03 10:11:51 -0600

    refactor: remove ref to sections and use scrollToSection

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,44 +1,20 @@
 "use client";
 
-import { Companies } from "./companies";
+import { About } from "./about";
+import { Companies } from "./experiences";
 import { Hero } from "./hero";
 import Info from "./info";
 import { Works } from "./works";
 import styles from "./Home.module.scss";
-import { useRef } from "react";
 
 const Home = () => {
-  const worksRef = useRef<HTMLDivElement>(null);
-  const infoRef = useRef<HTMLDivElement>(null);
-
-  const navigateToWorks = () => {
-    const header = document.querySelector("nav");
-    const linkOffset = worksRef.current?.offsetTop || 0;
-    let headerOffset = header?.offsetHeight || 0;
-
-    const sectionOffset = headerOffset < 90 ? 96 : 128;
-
-    window.scrollTo({
-      top: linkOffset - headerOffset - sectionOffset,
-      behavior: "smooth",
-    });
-  };
-
-  const navigateToInfo = () => {
-    const linkOffset = infoRef.current?.offsetTop || 0;
-    const headerOffset = -96;
-    window.scrollTo({
-      top: linkOffset + headerOffset,
-      behavior: "smooth",
-    });
-  };
-
   return (
     <div className={styles.home}>
-      <Hero onClick={navigateToWorks} />
-      {/* <Companies /> */}
-      <Works ref={worksRef} />
-      <Info ref={infoRef} />
+      <Hero />
+      <Companies />
+      <About />
+      <Works />
+      <Info />
     </div>
   );
 };
