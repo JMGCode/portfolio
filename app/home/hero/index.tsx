ed5dc7688ef8b6b8a75933a1dcda95784c72a5fa
Jesus Mezquiti <jmgcode@gmail.com>
2023-03-03 10:02:21 -0600

    refactor: replace ref by scrollToSection

--- app/home/hero/index.tsx
+++ app/home/hero/index.tsx
@@ -2,12 +2,10 @@ import ArrowDownCircle from "../../../components/Icons/arrowDownCircle";
 import Button from "../../../components/Button/Button";
 import { FC } from "react";
 import { primaryColor } from "../../../abstracts/colors";
+import { scrollToSection } from "../../../helpers";
 import styles from "./hero.module.scss";
 
-interface heroProps {
-  onClick: Function;
-}
-export const Hero: FC<heroProps> = ({ onClick }) => (
+export const Hero = () => (
   <div className={styles.container}>
     <h3 style={{ color: primaryColor }}>{`Hey there,I'm Jesus`}</h3>
     <h1>Web developer</h1>
@@ -18,7 +16,7 @@ export const Hero: FC<heroProps> = ({ onClick }) => (
       </p>
       <Button
         text="See my work"
-        onClick={() => onClick()}
+        onClick={() => scrollToSection("works-section")}
         icon={<ArrowDownCircle style={{ minWidth: "32px" }} />}
       />
     </div>
