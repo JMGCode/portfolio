d8591dbcf79541440eb674cc9f28b4c46747d9a3
Jesus Mezquiti <jmgcode@gmail.com>
2023-03-07 16:26:16 -0600

    feat: add offset to scrollTosection helper

--- helpers/scrollToSection.ts
+++ helpers/scrollToSection.ts
@@ -1,9 +1,9 @@
-const scrollToSection = (id: string) => {
+const scrollToSection = (id: string, offset: number = 100) => {
   const section = document.getElementById(id);
   // const offsetHeight = document.body.scrollHeight - section!!.scrollHeight - 96;
 
   window.scrollTo({
-    top: (section?.offsetTop || 0) - 96,
+    top: (section?.offsetTop || 0) - offset,
     behavior: "smooth",
   });
 };
