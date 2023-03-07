a02a6bf917cec3e706c6d072f78917cf73cd244f
Jesus Mezquiti <jmgcode@gmail.com>
2023-03-07 17:14:29 -0600

    fix: wrong env variable

--- nodemailer.ts
+++ nodemailer.ts
@@ -3,12 +3,12 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USSER,
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
 export const mailOptions = {
-  form: process.env.EMAIL_USSER,
-  to: process.env.EMAIL_USSER,
+  form: process.env.EMAIL_USER,
+  to: process.env.EMAIL_USER,
 };
