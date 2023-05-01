e72af5ef384b1564279639a1d044372db153c2a4
Jesus Mezquiti <jmgcode@gmail.com>
2023-04-30 22:47:12 -0600

    test: cors config by endpoint

--- next.config.js
+++ next.config.js
@@ -29,7 +29,23 @@ const nextConfig = {
   // headers: securityHeaders,
   headers: [
     {
-      source: "/api/spotify/*",
+      source: "/api/spotify/login",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
+      ],
+    },
+    {
+      source: "/api/spotiy/refresh",
       headers: [
         { key: "Access-Control-Allow-Credentials", value: "true" },
         { key: "Access-Control-Allow-Origin", value: "*" },
