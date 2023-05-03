2f93ae84b087a7859d72525193e7dce023975c93
Jesus Mezquiti <jmgcode@gmail.com>
2023-05-02 23:30:26 -0600

    test: new cors config

--- next.config.js
+++ next.config.js
@@ -26,26 +26,27 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "styles")],
   },
   experimental: { appDir: true },
-  // headers: securityHeaders,
-  headers: [
-    {
-      // matching all API routes
-      source: "/api/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value:
-            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      ],
-    },
-  ],
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
