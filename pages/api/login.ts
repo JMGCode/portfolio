fde69ce84fc05b3f33824f068011b8220a0df8a5
Jesus Mezquiti <jmgcode@gmail.com>
2023-05-12 21:05:28 -0600

    test: add cors wrapper

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import cors from "../../pages/api/cors";
+
 const SpotifyWebApi = require("spotify-web-api-node");
 
 type Data = {
@@ -14,49 +16,29 @@ const credentials = {
   clientSecret: process.env.CLIENT_SECRET,
 };
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Data | any>
-) => {
-  //set header first to allow request or origin domain (value can be different)
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, OPTIONS, DELETE"
-  );
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  cors(req, res, () => {
+    // Your request handling code here
+    if (req.method === "POST") {
+      const { code } = req.body;
+      const spotifyApi = new SpotifyWebApi(credentials);
 
-  //---- other code
-
-  //Preflight CORS handler
-  if (req.method === "OPTIONS") {
-    return res.status(200).json({
-      body: "OK",
-    });
-  }
-  if (req.method === "POST") {
-    const { code } = req.body;
-    const spotifyApi = new SpotifyWebApi(credentials);
-
-    spotifyApi
-      .authorizationCodeGrant(code)
-      .then((data: any) => {
-        res.json({
-          accessToken: data.body.access_token,
-          refreshToken: data.body.refresh_token,
-          expiresIn: data.body.expires_in,
+      spotifyApi
+        .authorizationCodeGrant(code)
+        .then((data: any) => {
+          res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+          });
+        })
+        .catch((error: any) => {
+          console.log(error);
+          return res.status(400);
         });
-      })
-      .catch((error: any) => {
-        console.log(error);
-        return res.status(400);
-      });
-  }
-  return res.status(400);
+    }
+    return res.status(400);
+  });
 };
 
 export default handler;
