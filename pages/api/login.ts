4ed56d9ec3ea0e01a0a9377488efb8f6f5d394d5
Jesus Mezquiti <jmgcode@gmail.com>
2023-05-12 20:56:47 -0600

    test: manual cors

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -14,7 +14,30 @@ const credentials = {
   clientSecret: process.env.CLIENT_SECRET,
 };
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data | any>
+) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+  );
+
+  //---- other code
+
+  //Preflight CORS handler
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({
+      body: "OK",
+    });
+  }
   if (req.method === "POST") {
     const { code } = req.body;
     const spotifyApi = new SpotifyWebApi(credentials);
