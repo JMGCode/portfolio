5d687dab44549f8190df542db17db3c8dd08253a
Jesus Mezquiti <jmgcode@gmail.com>
2023-05-29 13:07:15 -0600

    test: cors 03

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import cors from "../../../lib/cors";
 
 const SpotifyWebApi = require("spotify-web-api-node");
 
@@ -10,67 +9,38 @@ const credentials = {
 };
 
 export async function POST(request: Request) {
-  const { code } = await request.json();
+  // const { code } = await request.json();
 
-  const spotifyApi = new SpotifyWebApi(credentials);
+  // const spotifyApi = new SpotifyWebApi(credentials);
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data: any) => {
-      const { access_token, refresh_token, expires_in } = data.body;
+  // spotifyApi
+  //   .authorizationCodeGrant(code)
+  //   .then((data: any) => {
+  //     const { access_token, refresh_token, expires_in } = data.body;
 
-      // return NextResponse.json({
-      //   accessToken: access_token,
-      //   refreshToken: refresh_token,
-      //   expiresIn: expires_in,
-      // });
-      const bod = JSON.stringify({
-        accessToken: access_token,
-        refreshToken: refresh_token,
-        expiresIn: expires_in,
-      });
-
-      // return new Response(bod, {
-      //   status: 200,
-      //   headers: {
-      //     "Access-Control-Allow-Origin": "*",
-      //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      //     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      //   },
-      // });
-      return cors(
-        request,
-        new Response(bod, {
-          status: 200,
-          headers: { "Content-Type": "application/json" },
-        })
-      );
-    })
-    .catch((error: any) => {
-      console.log(error);
-      return new Response(null, {
-        status: 400,
-        statusText: "Bad Request",
-      });
-    });
+  //     return NextResponse.json({
+  //       accessToken: access_token,
+  //       refreshToken: refresh_token,
+  //       expiresIn: expires_in,
+  //     });
+  //   })
+  //   .catch((error: any) => {
+  //     console.log(error);
+  //     return new Response(null, {
+  //       status: 400,
+  //       statusText: "Bad Request",
+  //     });
+  //   });
+  return new Response("login --- all good", { status: 200 });
 }
 
-// export async function OPTIONS() {
-//   return new Response("all good", {
-//     status: 200,
-//     headers: {
-//       "Access-Control-Allow-Origin": "*",
-//       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
-//     },
-//   });
-// }
-
-export async function OPTIONS(request: Request) {
-  return cors(
-    request,
-    new Response(null, {
-      status: 204,
-    })
-  );
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
