d44c1c0cb05b954d91f1f0e866fc8f5765cd7c91
Jesus Mezquiti <jmgcode@gmail.com>
2023-05-31 09:25:41 -0500

    test: updata jmgcode url

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? ["https://jmgcode.com", "https://music-player.jmgcode.com"]
+    ? ["https://www.jmgcode.com", "https://music-player.jmgcode.com"]
     : ["http://localhost:3000", "https://music-player.jmgcode.com"];
 
 export function middleware(req: Request) {
