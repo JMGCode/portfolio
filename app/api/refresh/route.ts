aa2e90bff9ac53a93a8f530f6f73c86270d73ef6
Jesus Mezquiti <jmgcode@gmail.com>
2023-05-31 14:46:03 -0500

    test old login

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -1,3 +1,51 @@
+// import { NextResponse } from "next/server";
+
+// const SpotifyWebApi = require("spotify-web-api-node");
+
+// const credentials = {
+//   redirectUri: process.env.REDIRECT_URI,
+//   clientId: process.env.CLIENT_ID,
+//   clientSecret: process.env.CLIENT_SECRET,
+// };
+
+// export async function POST(request: Request) {
+//   const { refreshToken } = await request.json();
+
+//   const refreshCredentials = { ...credentials, refreshToken };
+//   const spotifyApi = new SpotifyWebApi(refreshCredentials);
+
+//   spotifyApi
+//     .refreshAccessToken()
+//     .then((data: any) => {
+//       console.log("spotify body refresh", data.body);
+//       const { access_token, expires_in } = data.body;
+
+//       return NextResponse.json({
+//         accessToken: access_token,
+//         expiresIn: expires_in,
+//         refreshToken,
+//       });
+//     })
+//     .catch((error: any) => {
+//       return new Response(null, {
+//         status: 400,
+//         statusText: "Bad Request",
+//       });
+//     });
+// }
+
+// export async function OPTIONS(request: Request) {
+//   const origin = request.headers.get("origin");
+//   return new Response("refresh options OK", {
+//     status: 200,
+//     headers: {
+//       "Access-Control-Allow-Origin": origin || "*",
+//       "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+//       "Access-Control-Allow-Headers": "Content-Type",
+//     },
+//   });
+// }
+
 import { NextResponse } from "next/server";
 
 const SpotifyWebApi = require("spotify-web-api-node");
@@ -9,39 +57,26 @@ const credentials = {
 };
 
 export async function POST(request: Request) {
-  const { refreshToken } = await request.json();
+  const { code } = await request.json();
 
-  const refreshCredentials = { ...credentials, refreshToken };
-  const spotifyApi = new SpotifyWebApi(refreshCredentials);
+  const spotifyApi = new SpotifyWebApi(credentials);
 
   spotifyApi
-    .refreshAccessToken()
+    .authorizationCodeGrant(code)
     .then((data: any) => {
-      console.log("spotify body refresh", data.body);
-      const { access_token, expires_in } = data.body;
+      const { access_token, refresh_token, expires_in } = data.body;
 
       return NextResponse.json({
         accessToken: access_token,
+        refreshToken: refresh_token,
         expiresIn: expires_in,
-        refreshToken,
       });
     })
     .catch((error: any) => {
+      console.log(error);
       return new Response(null, {
         status: 400,
         statusText: "Bad Request",
       });
     });
-}
-
-export async function OPTIONS(request: Request) {
-  const origin = request.headers.get("origin");
-  return new Response("refresh options OK", {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": origin || "*",
-      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-      "Access-Control-Allow-Headers": "Content-Type",
-    },
-  });
 }
