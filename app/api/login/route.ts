21be5dfc426774d51166b30917ff1de36bd0d085
Jesus Mezquiti <jmgcode@gmail.com>
2023-05-31 11:20:07 -0500

    test: spotify lib

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -14,53 +14,53 @@ export async function POST(request: Request) {
 
   const spotifyApi = new SpotifyWebApi(credentials);
 
-  return new NextResponse(
-    JSON.stringify({
-      code,
-      testToken: "test_token",
-      testRefresh: "test_refresh",
-      testExpiresIn: 123455,
-    }),
-    {
-      status: 200,
-      headers: {
-        "Access-Control-Allow-Origin": origin || "*",
-        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Content-Type": "application/jsond",
-      },
-    }
-  );
+  // return new NextResponse(
+  //   JSON.stringify({
+  //     code,
+  //     testToken: "test_token",
+  //     testRefresh: "test_refresh",
+  //     testExpiresIn: 123455,
+  //   }),
+  //   {
+  //     status: 200,
+  //     headers: {
+  //       "Access-Control-Allow-Origin": origin || "*",
+  //       "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  //       "Access-Control-Allow-Headers": "Content-Type",
+  //       "Content-Type": "application/jsond",
+  //     },
+  //   }
+  // );
 
-  // spotifyApi
-  //   .authorizationCodeGrant(code)
-  //   .then((data: any) => {
-  //     const { access_token, refresh_token, expires_in } = data.body;
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data: any) => {
+      const { access_token, refresh_token, expires_in } = data.body;
 
-  //     return new NextResponse(
-  //       JSON.stringify({
-  //         accessTokens: access_token,
-  //         refreshTokens: refresh_token,
-  //         expiresIns: expires_in,
-  //       }),
-  //       {
-  //         status: 200,
-  //         headers: {
-  //           "Access-Control-Allow-Origin": origin || "*",
-  //           "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  //           "Access-Control-Allow-Headers": "Content-Type",
-  //           "Content-Type": "application/json",
-  //         },
-  //       }
-  //     );
-  //   })
-  //   .catch((error: any) => {
-  //     console.log(error);
-  //     return new Response(null, {
-  //       status: 400,
-  //       statusText: "Bad Request, spotify",
-  //     });
-  //   });
+      return new NextResponse(
+        JSON.stringify({
+          accessTokens: access_token,
+          refreshTokens: refresh_token,
+          expiresIns: expires_in,
+        }),
+        {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": origin || "*",
+            "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    })
+    .catch((error: any) => {
+      console.log(error);
+      return new Response(null, {
+        status: 400,
+        statusText: "Bad Request, spotify",
+      });
+    });
 
   // const origin = request.headers.get("origin");
   // console.log("login post request", origin);
